d55c1791a1477682036c2fec7314b8d5803916d6
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2025-03-13 11:47:46 +0800

    Update diy-part1.sh

--- RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part1.sh
+++ RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part1.sh
@@ -11,10 +11,50 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# ttyd免登陆
+#sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+
+# Add a feed source(添加源)
+#添加插件源
+#echo -e "\nsrc-git Lei9008_package https://github.com/Lei9008/openwrt_package_Lite" >> feeds.conf.default
+## clone kiddin9/openwrt-packages仓库
+#git clone https://github.com/kiddin9/kwrt-packages package/new/openwrt-packages
+##添加自己的插件库
+#echo -e "\nsrc-git extraipk https://github.com/liker5092/extra_ipk" >> feeds.conf.default
+#echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
+
+
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#git clone https://github.com/messense/aliyundrive-webdav package/messense
-git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+
+# passwall
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 阿里云服务
+git clone https://github.com/messense/aliyundrive-webdav package/messense
+
+# 京东签到
+#git clone https://github.com/noiver/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+
+# 商店
+git clone https://github.com/linkease/istore-ui package/istore-ui
+git clone https://github.com/linkease/istore package/istore
+
+# 网易云解灰（天灵）
+#git clone https://github.com/UnblockNeteaseMusic/server package/server
+
+# 简单MESH
+git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
+
+# 集客AC控制器
+git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
+
+# AdguardHome-app
+git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+
+# MosDns
+git clone https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+
+# alist文件列表
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
